1f59f15bb322751296d4e378e14cad33287389f4
viva <vi2920va@gmail.com>
2022-06-17 11:17:10 +0900

    Add - O, X ㅂ바꾸는 로직 완성

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,31 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import Squere from "./Squere";
 import styled from "styled-components";
 
+export type Value = "X" | "O";
+export type BoardState = Value[];
+
 const Board: React.FC = () => {
-  const handleButtonPross = () => {};
+  const [squares, setSquares] = useState<BoardState>(Array(9).fill(null));
+  const [isNext, setIsNext] = useState(false);
+
+  const handleButtonPross = (i: number) => {
+    const boards = [...squares];
+    boards[i] = "X";
+
+    setSquares(boards);
+    setIsNext(true);
+    if (isNext) {
+      boards[i] = "O";
+      setSquares(boards);
+      setIsNext(false);
+    }
+  };
+
   const renderSquare = (i: number) => {
-    return <Squere onClick={handleButtonPross} value={i} />;
+    return <Squere onClick={() => handleButtonPross(i)} value={squares[i]} />;
+  };
+
+  const calculateWinner = (boardState: BoardState) => {
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < winningCombinations.length; i++) {
+      const [a, b, c] = winningCombinations[i];
+      if (
+        boardState[a] &&
+        boardState[a] === boardState[b] &&
+        boardState[a] === boardState[c]
+      ) {
+        return boardState[a];
+      }
+      return null;
+    }
   };
 
   return (
     <div>
       <StyledStats className="start">Next player : X</StyledStats>
-      <div className="board-row">
+      <StyleRow className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
         {renderSquare(2)}
-      </div>
-      <div className="board-row">
+      </StyleRow>
+      <StyleRow className="board-row">
         {renderSquare(3)}
         {renderSquare(4)}
         {renderSquare(5)}
-      </div>
-      <div className="board-row">
+      </StyleRow>
+      <StyleRow className="board-row">
         {renderSquare(6)}
         {renderSquare(7)}
         {renderSquare(8)}
-      </div>
+      </StyleRow>
     </div>
   );
 };
@@ -35,4 +77,8 @@ const StyledStats = styled.h1`
   font-size: 30px;
   font-weight: 600;
   text-align: center;
+`;
+
+export const StyleRow = styled.div`
+  display: flex;
 `;
--- src/components/Squere.tsx
+++ src/components/Squere.tsx
@@ -1,33 +1,28 @@
-import React, { useCallback } from "react";
+import React from "react";
 import styled from "styled-components";
 
 interface SquereProps {
-  value: number;
-  onClick: (value: number) => void;
+  value: number | string;
+  onClick: (i: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const Squere: React.FC<SquereProps> = ({ value }) => {
-  const handleButtonprocss = useCallback(
-    (value: number) => {
-      console.log("squere", value);
-    },
-    [value]
-  );
-  return (
-    <StyledSqureBtn onClick={() => handleButtonprocss(value)}>
-      {value}
-    </StyledSqureBtn>
-  );
+const Squere: React.FC<SquereProps> = ({ onClick, value }) => {
+  return <StyledSqureBtn onClick={onClick}>{value}</StyledSqureBtn>;
 };
 
 export default Squere;
 
 const StyledSqureBtn = styled.button`
-  width: 80px;
-  height: 80px;
+  width: 95px;
+  height: 95px;
+  padding: 0;
   border: 1px solid #999;
   font-size: 24px;
   font-weight: 700;
   background: #fff;
   cursor: pointer;
+
+  &:active {
+    padding: 0;
+  }
 `;
