1b2a9a5ac44cb58a44fec91097481d626bea4ce0
viva <vi2920va@gmail.com>
2022-06-20 10:23:28 +0900

    Add - Squere Button Style 추가

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -16,11 +16,23 @@ export default Square;
 const StyledSquareBtn = styled.button`
   width: 95px;
   height: 95px;
+  margin: 5px 5px;
   padding: 0;
-  border: 1px solid #999;
+  border: 1px solid #2d6898;
+  border-radius: 5px;
   font-size: 24px;
   font-weight: 700;
-  background: #fff;
+  background-image: linear-gradient(
+    bottom,
+    rgb(73, 132, 180) 0%,
+    rgb(97, 155, 203) 100%
+  );
+  background-color: #538fbe;
+  box-shadow: 0px 6px 0px #2b638f, 0px 3px 15px rgba(0, 0, 0, 0.4),
+    inset 0px 1px 0px rgba(255, 255, 255, 0.3),
+    inset 0px 0px 3px rgba(255, 255, 255, 0.5);
+  color: #fff;
+
   cursor: pointer;
 
   &:active {
